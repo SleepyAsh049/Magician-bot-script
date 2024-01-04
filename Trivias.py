79af73ea7ac3fc1fc87c4f7f4f83f5e60b31267c
SleepyAsh049 <80731577+SleepyAsh049@users.noreply.github.com>
2024-01-04 15:48:57 +0800

    Add files via upload

---
+++ Trivias.py
@@ -0,0 +1,18 @@
+T = [{
+        'question': 'What is the capital of France?',
+        'options': ['Paris', 'Madrid', 'Rome', 'London'],
+        'answer': 'Paris'
+    },
+    {
+        'question': 'What is the largest planet in our solar system?',
+        'options': ['Jupiter', 'Mars', 'Venus', 'Saturn'],
+        'answer': 'Jupiter'
+    },
+    {
+        'question': 'Who painted the Mona Lisa?',
+        'options': ['Leonardo da Vinci', 'Pablo Picasso', 'Vincent van Gogh', 'Michelangelo'],
+        'answer': 'Leonardo da Vinci'
+    }
+]
+
+    
